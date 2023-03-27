f18ebf5f270a5cce53d6da4ffb24a01b3443cdaf
Aleksandr Pisarev <a.pisarev93@gmail.com>
2023-03-27 19:05:19 +0300

    add Poetry

---
+++ virtual environment/Poetry.py
@@ -0,0 +1,30 @@
+# # Windows
+# pip install poetry
+
+# # macOS
+# brew install poetry
+
+# # Linux, macOS
+# curl -sSL https://install.python-poetry.org | python3 -
+# export PATH="$HOME/.local/bin:$PATH"
+
+# poetry env use python3.8
+#  — создать виртуальное окружение, указав версию интерпретатора.
+
+# poetry shell
+#  — активировать виртуальное окружение.
+
+# exit
+#  — выйти из виртуального окружения.
+
+# poetry run python main.py
+#  — запустить файл python без активации виртуального окружения.
+
+# poetry remove requests
+#  — удалить зависимость из проекта.
+
+# poetry show --tree
+#  — посмотреть всё дерево зависимостей.
+
+# poetry show --latest
+#  — просмотреть, последние ли версии используются в проекте.
---
+++ virtual environment/VE_venv.py
@@ -0,0 +1,20 @@
+# # Для Windows
+# python -m venv venv
+
+# # Для Linux, macOS
+# python3 -m venv venv
+
+# venv/Scripts/python
+#  — запуск python.
+
+# venv/Scripts/pip install pandas
+#  — установка пакета.
+
+# venv/Scripts/activate.bat
+#  — активация окружения.
+
+# python
+#  — запуск python в окружении.
+
+# pip install pandas
+#  — установка пакета в окружение.
